0f6e271db2758ea5c1c178dae240944d87555529
Lukasz Margielewski <lukasz.margielewski@gmail.com>
2016-11-26 14:25:08 +0100

    code align

--- SegmentationFault11/Classes/Objective-C/NetworkResponseProcessor.h
+++ SegmentationFault11/Classes/Objective-C/NetworkResponseProcessor.h
@@ -12,10 +12,10 @@
 @interface NetworkResponseProcessor : NSObject
 
 - (nullable id)process:(nullable NSURLResponse *)response
-                                 data:(nullable NSData *)data
-                         networkError:(nullable NSError *)networkError
-                                error:(NSError * _Nonnull * _Nullable)error; // <= Error: "Segmentation Fault 11".
-                             // error:(NSError * _Nullable * _Nullable)error;  // <= Compiles.
+                  data:(nullable NSData *)data
+          networkError:(nullable NSError *)networkError
+                 error:(NSError * _Nonnull * _Nullable)error; // <= Error: "Segmentation Fault 11".
+              // error:(NSError * _Nullable * _Nullable)error;  // <= Compiles.
 /*
  The innermost NSError pointer is required to be nullable if you want proper writeback semantics.
  The Swift compiler assumes you know this and crashes instead of emitting a diagnostic (bug!).
